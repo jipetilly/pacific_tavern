84914e8f28c64201fc3605d3b73a491999c750dc
jipetilly <jipetilly@gmail.com>
2021-02-15 08:39:52 +0100

    version lundi 15-02

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+// SWIPER PACIFIC IMAGES
+
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+
+// Smooth Scroll
+
